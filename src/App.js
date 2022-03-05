c3b8c48ba0ef17714dfc4f7f694df4f78f90c136
mmad93 <m.taba1508@gmail.com>
2022-03-04 20:02:36 -0800

    added the component folder and the first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import NavBar from './Components/NavBar';
 
 import React from 'react'
 
 const App = () => {
   return (
-    <div>The challenege 1</div>
+    <div>
+      <h1>this my first challenge</h1>
+      <NavBar/>
+    </div>
   )
 }
 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>this is my navbar</div>
+  )
+}
+
+export default NavBar
